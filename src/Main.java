bbc60382d9b4acb7f6e1a4c450f3dc4c9e4c6c57
dhanush-png-3011 <dhanushguptha30@gmail.com>
2026-03-11 08:33:31 +0530

    UC4-added

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,22 @@ public class Main {
     public static void main(String[] args){
 
         String input = "madam";
-        String reversed = "";
+        char[] chars = input.toCharArray();
 
-        for (int i = input.length() - 1; i >= 0; i--) {
-            reversed = reversed + input.charAt(i);
+        int left = 0;
+        int right = chars.length - 1;
+        boolean isPalindrome = true;
+
+        while (left < right) {
+            if (chars[left] != chars[right]) {
+                isPalindrome = false;
+                break;
+            }
+            left++;
+            right--;
         }
 
-        if (input.equals(reversed)) {
+        if (isPalindrome) {
             System.out.println(input + " is a Palindrome.");
         } else {
             System.out.println(input + " is not a Palindrome.");
